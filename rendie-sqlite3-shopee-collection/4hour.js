0d0507fd26e19cce75538218b6ee09ac64171e49
rendie <121609433+rendie-com@users.noreply.github.com>
2026-02-02 00:48:28 +0800

    Update 4hour.js

--- rendie-sqlite3-shopee-collection/4hour.js
+++ rendie-sqlite3-shopee-collection/4hour.js
@@ -53,7 +53,7 @@ import path from 'path';
   await page.goto(url);
   /////////////////////////////////// 
   let count = 0;
-  let total =  (60*50) / 10//=(60*50)最多运行50分钟
+  let total =  (60*60) / 10//最多运行60分钟
   let Enable = true;
   while (Enable) {
     count++
