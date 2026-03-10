2ab6c7b7e6befe8515c751f95ba9605844ea6004
rendie <121609433+rendie-com@users.noreply.github.com>
2026-03-10 20:47:25 +0800

    Update 1hour.js

--- rendie-sqlite3-1688/1hour.js
+++ rendie-sqlite3-1688/1hour.js
@@ -53,7 +53,7 @@ import path from 'path';
   await page.goto(url);
   /////////////////////////////////// 
   let count = 0;
-  let total =  (60*40) / 10//最多运行40分钟
+  let total =  (60*4) / 10//最多运行4分钟
   let Enable = true;
   while (Enable) {
     count++
